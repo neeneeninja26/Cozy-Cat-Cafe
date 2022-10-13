9592fc675b8777f1a4eba4d78e5d7f617927a0bd
Renee <renee.hansen2607@gmail.com>
2022-10-14 11:22:03 +1300

    Add project files.

---
+++ WebAssignment/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAssignment.Models
+{
+    public class Category
+    {
+    }
+}
